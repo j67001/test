32f315fd641a89fbb5495fa03f780f370faa8e25
j67001 <95325783+j67001@users.noreply.github.com>
2023-08-19 15:06:26 +0800

    Update 哔哩影视.js

--- 哔哩影视.js
+++ 哔哩影视.js
@@ -12,7 +12,6 @@ var rule = {
     headers:{
         'User-Agent':'PC_UA',
         "Referer": "https://www.bilibili.com",
-        //"Cookie":"$bili_cookie"
         "Cookie": "SESSDATA=7624af93%2C1696008331%2C862c8%2A42; bili_jct=141a474ef3ce8cf2fedf384e68f6625d; DedeUserID=3493271303096985; DedeUserID__ckMd5=212a836c164605b7"
     },
     },
