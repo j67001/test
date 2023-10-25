58deaa2f42e92d87ce257a82fdd12b8ae82702d5
j67001 <95325783+j67001@users.noreply.github.com>
2023-10-26 00:12:43 +0800

    Update 剧迷.js

--- 剧迷.js
+++ 剧迷.js
@@ -1,9 +1,11 @@
 muban.首图.二级.title = 'h1&&Text;p.data:eq(0)&&Text';
-muban.首图.二级.desc = 'p.data--span:eq(3)&&Text;;;p.data--span:eq(1)&&Text;p.data--span:eq(2)&&Text';
+muban.首图2.二级.desc = '.data:eq(1)&&Text;;;.data:eq(6)&&Text;.data:eq(5)&&Text';
+
 muban.首图2.二级.tabs = '.stui-pannel__head h3';
 var rule = Object.assign(muban.首图2,{
 	title:'剧迷',
 /*
+muban.首图.二级.desc = 'p.data--span:eq(3)&&Text;;;p.data--span:eq(1)&&Text;p.data--span:eq(2)&&Text';
 muban.首图.二级.tabs = '.myui-panel__head.bottom-line h3';
 var rule = {
 	title:'剧迷',
