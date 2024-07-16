976298ca0f0280f6e2b55ad31a2ae35be210396e
j67001 <95325783+j67001@users.noreply.github.com>
2024-07-16 13:50:32 +0800

    Update drpy.js

--- drpy.js
+++ drpy.js
@@ -33,7 +33,6 @@ var rule = {
 {'key': 'area', 'name': '地区', 'value': [{'n': '全部', 'v': '全部'}, {'n': '华语', 'v': '华语'}, {'n': '欧美', 'v': '欧美'}, {'n': '韩国', 'v': '韩国'}, {'n': '日本', 'v': '日本'}]} 
 ], 
 'tv_hot': [
-{'key': 'sort', 'name': '排序', 'value': [{'n': '热度', 'v': 'recommend'}, {'n': '最新', 'v': 'time'}, {'n': '评分', 'v': 'rank'}]}, 
 {'key': 'type', 'name': '分类', 'value': [{'n': '综合', 'v': 'tv_hot'}, {'n': '国产剧', 'v': 'tv_domestic'}, {'n': '欧美剧', 'v': 'tv_american'}, {'n': '日剧', 'v': 'tv_japanese'}, {'n': '韩剧', 'v': 'tv_korean'}, {'n': '动画', 'v': 'tv_animation'}]}
 ], 
 'show_hot': [
