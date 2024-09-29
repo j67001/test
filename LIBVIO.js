d901ac2232fd38d3d7d83429989397a87b4c0654
j67001 <95325783+j67001@users.noreply.github.com>
2024-09-30 00:34:43 +0800

    Update LIBVIO.js

--- LIBVIO.js
+++ LIBVIO.js
@@ -5,7 +5,9 @@ muban.首图2.二级.content = '.detail-content&&Text'
 var rule = {
 	title:'LIBVIO',
 	模板:'首图2',
-	host:'https://libvio.cc/',
+
+	host:'https://libvio.link',
+	//host:'https://libvio.cc/',
 	// host:'https://libvio.app',
 	// hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(0)&&a:eq(0)&&href");print(src);HOST=src',
 	// url:'/type/fyclass-fypage.html',
