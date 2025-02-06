07aaad740421673e354d3f7c052d60f74bc175dd
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-06 19:15:22 +0800

    Update 小寶影院XBPQ.js

--- 小寶影院XBPQ.js
+++ 小寶影院XBPQ.js
@@ -6,7 +6,7 @@
     "主页url": "https://xiaoxintv.cc/index.php",
     "首页": "100",
     "起始页": "1",
-    "分类url": "/vod/show/area/{area}/by/{by}/class/{class}/id/{cateId}/lang/{lang}/letter/{letter}/page/{catePg}/year/{year}.html",
+    "分类url": "/vod/show/{area}{by}{class}/id/{cateId}{lang}{catePg}{year}.html",
     "分类": "电视剧&电影&动漫&综艺&短剧",
     "分类值": "6&7&5&3&64",
 
