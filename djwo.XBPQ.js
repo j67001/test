2b8a1ab9792cd6fa50da4da5c14c6450c7468e97
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-08 00:53:42 +0800

    Update djwo.XBPQ.js

--- djwo.XBPQ.js
+++ djwo.XBPQ.js
@@ -4,15 +4,23 @@
     "分类url": "https://www.djwo.cc/show/1/by/{cateId}/page/{catePg}",
     "分类": "时间&人气&评分",
     "分类值": "time&hits&score",
-
+        
+  "链接": "href=\"&&\"",
+  "搜索模式": "1",
+  "搜索url": "/search/--/?wd={wd}",
+  "搜索数组": "vod-list row\">&&</li>",
+  "搜索图片": "data-original=\"&&\"",
+  "搜索标题": "title=\"&&\"",
+  "搜索链接": "<a href=\"&&\"",
+/*
+  "线路数组": "<span class=\"pull-right\">&&/h3>",
+  "线路标题": "<h3>&&<",
+  "播放数组": "<ul class=\"myui-content__playlist&&</ul>",
   "数组": "data-&&</h4>",
   "图片": "original=\"&&\"",
   "标题": "title=\"&&\"",
   "副标题": "text-right\">&&</span>",
-  "链接": "href=\"&&\"",
-  "搜索模式": "1",
-  "搜索url": "/search/--/?wd={wd}",
-  "搜索数组": "myui-vodlist__box\">&&</li>",
+  "搜索数组": "vod-list row\">&&</li>",
   "搜索图片": "data-original=\"&&\"",
   "搜索标题": "title=\"&&\"",
   "搜索链接": "<a href=\"&&\"",
@@ -23,10 +31,7 @@
   "导演": "导演：&&</p>",
   "主演": "主演：&&</p>",
   "简介": "display: none;\">&&</span>",
-  "线路数组": "<span class=\"pull-right\">&&/h3>",
-  "线路标题": "<h3>&&<",
-  "播放数组": "<ul class=\"myui-content__playlist&&</ul>",
   "过滤词":"=http",
-
+*/
     "筛选": "1"
 }
