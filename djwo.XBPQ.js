0353ab2b7f00a55e669e54662f4d004594d6889e
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-08 00:01:29 +0800

    Create djwo.XBPQ.js

---
+++ djwo.XBPQ.js
@@ -0,0 +1,34 @@
+{
+    "站名": "djwo",
+    "主页url": "https://www.djwo.cc",
+    "分类url": "https://www.djwo.cc/show/{cateId}{by}/page/{catePg}",
+    "分类": "短剧",
+    "分类值": "1",
+/*
+  "数组": "data-&&</h4>",
+  "图片": "original=\"&&\"",
+  "标题": "title=\"&&\"",
+  "副标题": "text-right\">&&</span>",
+  "链接": "href=\"&&\"",
+  "搜索模式": "1",
+  "搜索url": "/search.html?wd={wd}",
+  "搜索数组": "myui-vodlist__box\">&&</li>",
+  "搜索图片": "data-original=\"&&\"",
+  "搜索标题": "title=\"&&\"",
+  "搜索链接": "<a href=\"&&\"",
+  "搜索副标题": "text-right\">&&</span>",
+  "影片年代": "年份：&&</p>",
+  "影片地区": "地区：&&/",
+  "影片类型": "类型：&&/",
+  "导演": "导演：&&</p>",
+  "主演": "主演：&&</p>",
+  "简介": "display: none;\">&&</span>",
+  "线路数组": "<span class=\"pull-right\">&&/h3>",
+  "线路标题": "<h3>&&<",
+  "播放数组": "<ul class=\"myui-content__playlist&&</ul>",
+  "过滤词":"=http",
+*/
+    "筛选":{
+"1":[{"key":"by","name":"排序","value":[{"n":"時間","v":"/by/time"},{"n":"人氣","v":"/by/hits"},{"n":"月人氣","v":"/by/score"}]}]
+    }
+}
