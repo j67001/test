55a6826d7a4370ef2c7009c9c00b1df1db2c00c8
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-08 01:58:55 +0800

    Update djwo.XBPQ.js

--- djwo.XBPQ.js
+++ djwo.XBPQ.js
@@ -1,9 +1,9 @@
 {
     "站名": "djwo",
     "主页url": "https://www.djwo.cc",
-    "分类url": "https://www.djwo.cc/show/1/by/{cateId}/page/{catePg}",
-    "分类": "时间&人气&评分",
-    "分类值": "time&hits&score",
+    "分类url": "https://www.djwo.cc/show/{cateId}{by}{year}/page/{catePg}",
+    "分类": "短剧",
+    "分类值": "1",
         
   "链接": "href=\"&&\"",
   "搜索模式": "1",
@@ -16,8 +16,7 @@
 /*
   "线路数组": "<span class=\"pull-right\">&&/h3>",
   "线路标题": "<h3>&&<",
- 
-  "数组": "data-&&</h4>",
+   "数组": "data-&&</h4>",
   "图片": "original=\"&&\"",
   "标题": "title=\"&&\"",
   "副标题": "text-right\">&&</span>",
@@ -34,5 +33,7 @@
   "简介": "display: none;\">&&</span>",
   "过滤词":"=http",
 */
-    "筛选": "1"
+    "筛选": {
+"1":[{"key":"by","name":"排序","value":[{"n":"时间","v":"/by/time"},{"n":"人气","v":"/by/hits"},{"n":"评分","v":"/by/score"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2025","v":"/year/2025"},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"}]}]
+    }
 }
