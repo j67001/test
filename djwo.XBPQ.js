a23fe5a750b918e26c082fcb2c6712ce1411a463
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-08 00:12:14 +0800

    Update djwo.XBPQ.js

--- djwo.XBPQ.js
+++ djwo.XBPQ.js
@@ -1,9 +1,9 @@
 {
     "站名": "djwo",
     "主页url": "https://www.djwo.cc",
-    "分类url": "https://www.djwo.cc/show/1{by}/page/{catePg}",
-    "分类": "短剧",
-    "分类值": "1",
+    "分类url": "https://www.djwo.cc/show/1/by/{cateId}/page/{catePg}",
+    "分类": "时间&人气&评分",
+    "分类值": "time&hits&score",
 /*
   "数组": "data-&&</h4>",
   "图片": "original=\"&&\"",
@@ -28,7 +28,5 @@
   "播放数组": "<ul class=\"myui-content__playlist&&</ul>",
   "过滤词":"=http",
 */
-    "筛选":{
-"1":[{"key":"by","name":"排序","value":[{"n":"時間","v":"/by/time"},{"n":"人氣","v":"/by/hits"},{"n":"月人氣","v":"/by/score"}]}]
-    }
+    "筛选": "1",
 }
