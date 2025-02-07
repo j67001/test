133463099cef1d6cef96282044d973fc193c5f8f
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-08 02:00:38 +0800

    Update djwo.XBPQ.js

--- djwo.XBPQ.js
+++ djwo.XBPQ.js
@@ -1,7 +1,7 @@
 {
     "站名": "djwo",
     "主页url": "https://www.djwo.cc",
-    "分类url": "https://www.djwo.cc/show/{cateId}{by}{year}/page/{catePg}",
+    "分类url": "https://www.djwo.cc/show/1{by}{year}/page/{catePg}",
     "分类": "短剧",
     "分类值": "1",
         
