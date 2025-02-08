7f40f357a320aa6c2e9a940bf15e0246691fa96a
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-08 11:11:29 +0800

    Update djwo.js

--- djwo.js
+++ djwo.js
@@ -1,43 +1,38 @@
 
-muban.首图2.二级.title = 'h1&&Text;p.data:eq(0)&&Text';
-muban.首图2.二级.desc = 'p.data--span:eq(3)&&Text;;;p.data--span:eq(1)&&Text;p.data--span:eq(2)&&Text';
-muban.首图2.二级.tabs = '.stui-pannel__head h3';
-var rule = {
-	title:'djwo',
-	模板:'首图2',
-	host:'https://www.djwo.cc',
-	url:'/show/fyfilter',
-	filterable:1,//是否启用分类筛选,
-	filter_url:'{{fl.cateId}}{{fl.by}}{{fl.year}}/page/fypage',
-	filter: {
-		"1":[{"key":"by","name":"排序","value":[{"n":"时间","v":"/by/time"},{"n":"人气","v":"/by/hits"},{"n":"评分","v":"/by/score"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2025","v":"/year/2025"},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"}]}]
-	},
-	filter_def:{
-		1:{cateId:'1',by:'/by/time'}
-	},
-/*	
-	filter: {
-		"1":[{"key":"cateId","name":"類型","value":[{"n":"全部","v":"1"},{"n":"劇情片","v":"11"},{"n":"動作片","v":"6"},{"n":"科幻片","v":"9"},{"n":"喜劇片","v":"7"},{"n":"愛情片","v":"8"},{"n":"戰爭片","v":"12"},{"n":"恐怖片","v":"10"},{"n":"動畫電影","v":"24"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":""},{"n":"美國","v":"/area/美國"},{"n":"韓國","v":"/area/韓國"},{"n":"日本","v":"/area/日本"},{"n":"台灣","v":"/area/臺灣"},{"n":"大陸","v":"/area/大陸"},{"n":"香港","v":"/area/香港"},{"n":"歐美","v":"/area/歐美"},{"n":"泰國","v":"/area/泰國"},{"n":"英國","v":"/area/英國"},{"n":"法國","v":"/area/法國"},{"n":"新加坡","v":"/area/新加坡"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2025","v":"/year/2025"},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"}]},{"key":"by","name":"排序","value":[{"n":"按時間","v":"/by/time"},{"n":"周人氣","v":"/by/hits_week"},{"n":"月人氣","v":"/by/hits_month"}]}],
-		"2":[{"key":"cateId","name":"類型","value":[{"n":"全部","v":"2"},{"n":"美劇","v":"16"},{"n":"韓劇","v":"20"},{"n":"日劇","v":"15"},{"n":"台劇","v":"14"},{"n":"陸劇","v":"13"},{"n":"港劇","v":"21"},{"n":"海外劇","v":"31"},{"n":"紀錄片","v":"22"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":""},{"n":"美國","v":"/area/美國"},{"n":"韓國","v":"/area/韓國"},{"n":"日本","v":"/area/日本"},{"n":"台灣","v":"/area/臺灣"},{"n":"大陸","v":"/area/大陸"},{"n":"香港","v":"/area/香港"},{"n":"歐美","v":"/area/歐美"},{"n":"泰國","v":"/area/泰國"},{"n":"英國","v":"/area/英國"},{"n":"法國","v":"/area/法國"},{"n":"新加坡","v":"/area/新加坡"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2025","v":"/year/2025"},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"}]},{"key":"by","name":"排序","value":[{"n":"按時間","v":"/by/time"},{"n":"周人氣","v":"/by/hits_week"},{"n":"月人氣","v":"/by/hits_month"}]}],
-		"29":[{"key":"cateId","name":"類型","value":[{"n":"全部","v":"29"},{"n":"綜藝","v":"29"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":""},{"n":"美國","v":"/area/美國"},{"n":"韓國","v":"/area/韓國"},{"n":"日本","v":"/area/日本"},{"n":"台灣","v":"/area/臺灣"},{"n":"大陸","v":"/area/中國大陸"},{"n":"香港","v":"/area/香港"},{"n":"歐美","v":"/area/歐美"},{"n":"英國","v":"/area/英國"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2025","v":"/year/2025"},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"}]},{"key":"by","name":"排序","value":[{"n":"按時間","v":"/by/time"},{"n":"周人氣","v":"/by/hits_week"},{"n":"月人氣","v":"/by/hits_month"}]}],
-		"30":[{"key":"cateId","name":"類型","value":[{"n":"全部","v":"30"},{"n":"動漫","v":"30"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":""},{"n":"日本","v":"/area/日本"},{"n":"美國","v":"/area/美國"},{"n":"歐美","v":"/area/歐美"},{"n":"英國","v":"/area/英國"},{"n":"韓國","v":"/area/韓國"},{"n":"大陸","v":"/area/大陸"},{"n":"台灣","v":"/area/臺灣"},{"n":"香港","v":"/area/香港"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2025","v":"/year/2025"},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"}]},{"key":"by","name":"排序","value":[{"n":"按時間","v":"/by/time"},{"n":"周人氣","v":"/by/hits_week"},{"n":"月人氣","v":"/by/hits_month"}]}],
-	},
-	filter_def:{
-		1:{cateId:'1',by:'/by/time'},
-		2:{cateId:'2',by:'/by/time'},
-		29:{cateId:'29',by:'/by/time'},
-		30:{cateId:'30',by:'/by/time'}
-	},
- */
-	searchUrl:'/search/--/?wd=**',
-	searchable:2,
-	quickSearch:0,
-	class_parse: '.vod-list row li;a&&Text;a&&href;.*/(.*?)/',
-	一级:'.vod-list li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-	play_parse:true,
-	lazy:'',
-}
+var rule={
+    title:'djwo',
+    host:'https://www.djwo.cc,
+    //host:'https://www.tangrenjie.one',
+    // url:'/vod/show/id/fyclass/page/fypage.html',
+    url:'/show/fyfilter.html',
+    filterable:1,//是否启用分类筛选,
+    filter_url:'{{fl.cateId}}{{fl.by}}{{fl.year}}/page/fypage',
+    filter: {
+        "1":[{"key":"by","name":"排序","value":[{"n":"时间","v":"/by/time"},{"n":"人气","v":"/by/hits"},{"n":"评分","v":"/by/score"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2025","v":"/year/2025"},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"}]}]
+    },
+    filter_def:{
+        1:{cateId:'1',by:'/by/time'}
+    },
 
-//	class_parse: 'ul.myui-header__menu li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
-//	一级:'.myui-vodlist li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-//}
+    searchUrl:'/search/--/?wd=**',
+    searchable:2,//是否启用全局搜索,
+    headers:{
+        'User-Agent':'UC_UA',
+    },
+    class_parse:'.vod-list row&&li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'ul.vodlist.vodlist_wi;li;*;*;*;*',
+    double:true, // 推荐内容是否双层定位
+    一级:'li.vodlist_item;a&&title;a&&data-original;.pic_text.text_right&&Text;a&&href',
+    二级:{
+        "title":".lazyload&&title;.data:eq(3)--span&&Text",
+        "img":".lazyload&&data-original",
+        "desc":".data:eq(2)span&&Text;;;.data--span:eq(0)&&Text;.data--span:eq(1)&&Text",
+        "content":".content&&Text",
+        "tabs":"#NumTab a",
+        "lists":".play_list_box:eq(#id) .playlist_full li"
+    },
+    搜索:'body .searchlist_item;*;.vodlist_thumb&&data-original;*;*',
+}
