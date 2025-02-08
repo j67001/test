8205792de818d60bb9b16837dacba973dbbca757
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-08 15:51:11 +0800

    Update hitv.XBPQ.js

--- hitv.XBPQ.js
+++ hitv.XBPQ.js
@@ -8,6 +8,7 @@
   "播放数组": "class=\"*searis-box&&</ul",
   "简介": "class=\"video-infos-desc overflow-hidden&&</p>",
   "线路标题": "Hi視頻",
+  "选集标题链接是否Jsoup写法":"1",
   "是否反转选集序列":"1",
   "搜索url": "/search/{wd}.html",
 /*
