2a2428b39896ecd9ef669614444ba42cc6fc90de
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-09 23:32:36 +0800

    Create MFFM.XBPQ.js

---
+++ MFFM.XBPQ.js
@@ -0,0 +1,42 @@
+{
+    "站名": "hitvmffm",
+    "主页url": "https://www.movieffm.net",
+    "分类url": "https://www.movieffm.net/{cateId}",
+    "分类": "Netflix電影&Netflix影集&電影&連續劇&動漫&动漫&綜藝",
+    "分类值": "cats/netflix/&network/netflix/&movies/&drama/&drama/?tvtype=anime&drama/?tvtype=10",
+
+  "播放数组": "class=\"*searis-box&&</ul",
+  "简介": "class=\"video-infos-desc overflow-hidden&&</p>",
+  "线路标题": "Hi視頻",
+  "是否反转选集序列":"0",
+  "搜索url": "/search/{wd}.html",
+/*
+  "链接": "href=\"&&\"",
+  "搜索模式": "1",
+
+  "搜索数组": "vod-list row\">&&</li>",
+  "搜索图片": "data-original=\"&&\"",
+  "搜索标题": "title=\"&&\"",
+  "搜索链接": "<a href=\"&&\"",
+ 
+  "线路数组": "<span class=\"pull-right\">&&/h3>",
+  "线路标题": "<h3>&&<",
+  "数组": "data-&&</h4>",
+  "图片": "original=\"&&\"",
+  "标题": "title=\"&&\"",
+  "副标题": "text-right\">&&</span>",
+  "搜索数组": "vod-list row\">&&</li>",
+  "搜索图片": "data-original=\"&&\"",
+  "搜索标题": "title=\"&&\"",
+  "搜索链接": "<a href=\"&&\"",
+  "搜索副标题": "text-right\">&&</span>",
+  "影片年代": "年份：&&</p>",
+  "影片地区": "地区：&&/",
+  "影片类型": "类型：&&/",
+  "导演": "导演：&&</p>",
+  "主演": "主演：&&</p>",
+  "简介": "display: none;\">&&</span>",
+  "过滤词":"=http",
+*/
+    "筛选": "1"
+}
