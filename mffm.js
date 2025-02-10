dc6c39718decc9d3ede6f046a6988995a73d6626
j67001 <95325783+j67001@users.noreply.github.com>
2025-02-10 16:46:49 +0800

    Create mffm.js

---
+++ mffm.js
@@ -0,0 +1,23 @@
+var rule={
+            title: 'movieffm',
+            host: 'https://www.movieffm.net/',
+            url:'/fyclass/fypage.html',
+          //https://www.movieffm.net/xssearch?q=%E5%90%8D
+            searchUrl: '/xssearch?q=**',    
+            searchable: 2,//是否启用全局搜索,
+            quickSearch: 0,//是否启用快速搜索,
+            filterable: 0,//是否启用分类筛选,
+            headers:{'User-Agent':'MOBILE_UA'},
+          class_parse: '.mi_nav&&li;a&&Text;a&&href;/(\\d+)',
+            play_parse: true,
+            lazy: '',
+            limit: 6,  
+          推荐: '.item;h3&&Text;img&&data-lazy-src;.rating&&Text;a&&href',
+          一级: '.item;h4&&Text;img&&data-lazy-src;.rating&&Text;a&&href',
+          二级: {
+                "title": "h1&&Text",
+                "content": ".wp-content&&Text",
+                "tabs": ".options&&span.title",//解析源
+                "lists": ".options&&ul:eq(#id) li"
+            },
+ }
