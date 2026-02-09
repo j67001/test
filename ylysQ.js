1718388babcaee953400e456afe168feb816f0f2
j67001 <95325783+j67001@users.noreply.github.com>
2026-02-09 15:26:37 +0800

    Update ylysQ.js

--- ylysQ.js
+++ ylysQ.js
@@ -1,3 +1,5 @@
+import 'assets://js/lib/crypto-js.js';
+
 let host = 'https://www.ylys.tv';
 const headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.61 Safari/537.36",
