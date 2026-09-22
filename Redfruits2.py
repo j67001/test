58d2e04aa83fdc9d5f1fdbe381685d0b3e51225f
j67001 <95325783+j67001@users.noreply.github.com>
2026-09-22 15:38:56 +0800

    Update Redfruits2.py

--- Redfruits2.py
+++ Redfruits2.py
@@ -4414,8 +4414,8 @@ class Spider(Spider):
         # 將 content_type 分類參數以及 route 的 canonicalPath 整合進去
         'real-drama': {'type_name': '真人剧',  'kind': 'category', 'query': 'tab=1&content_type=1&sort_type=1', 'route_path': '/category/real-drama'},
         'ai-drama': {'type_name': 'AI剧',   'kind': 'category', 'query': 'tab=1&content_type=4&sort_type=1', 'route_path': '/category/ai-drama'},
-        'comic-drama': {'type_name': '漫剧',   'kind': 'category', 'query': 'tab=1&content_type=2&sort_type=1', 'route_path': '/category/comic-drama'},
-        'comic': {'type_name': '漫画',  'kind': 'category', 'query': 'tab=1&content_type=3&sort_type=1', 'route_path': '/category/comic'},
+        'comic-drama': {'type_name': '漫剧',   'kind': 'category', 'query': 'tab=1&content_type=3&sort_type=1', 'route_path': '/category/comic-drama'},
+        'comic': {'type_name': '漫画',  'kind': 'category', 'query': 'tab=2&content_type=2&sort_type=1', 'route_path': '/category/comic'},
         'rank_hot':   {'type_name': '红果热播榜',    'kind': 'rank', 'route': 'hot-drama'},
         'rank_human': {'type_name': '真人剧热播榜',  'kind': 'rank', 'route': 'hot-real-drama'},
         'rank_comic': {'type_name': '漫剧热播榜',    'kind': 'rank', 'route': 'hot-comic-drama'},
