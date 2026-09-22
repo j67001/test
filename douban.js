47cd0137f17c268ab123ecce90d1770d801aa78b
j67001 <95325783+j67001@users.noreply.github.com>
2026-09-23 02:00:51 +0800

    Update import paths for local module references

--- douban.js
+++ douban.js
@@ -6,9 +6,9 @@
 * @Software : Samples
 * @Desc     :
 */
-import {VodShort} from "../lib/vod.js";
+import {VodShort} from "./lib/vod.js";
 import {Spider} from "./spider.js";
-import {_, Crypto} from "../lib/cat.js";
+import {_, Crypto} from "./lib/cat.js";
 
 class DoubanSpider extends Spider {
     constructor() {
